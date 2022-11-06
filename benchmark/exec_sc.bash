b21f378a8ba1b3c7d9ae4d8b652aab3657798071
OthmanSEBTI <seb.othmane@gmail.com>
2022-11-05 20:45:31 -0400

    Automatic script for execute social network

---
+++ benchmark/exec_sc.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BUILD=false
+
+while getopts ib flag;
+do
+    case "${flag}" in
+        b) BUILD=true;;
+        i) INPUT=${OPTARG};;
+    esac
+done
+
+# Compile source code
+if [ $BUILD = true ]; then
+    map-reduce-social-network/benchmark/build_sc.bash
+fi
+
+# Setting the dataset into input hdfs
+if [[ ! -z $INPUT ]]; then
+   hadoop/bin/hdfs dfs -rm -r input
+   hadoop/bin/hdfs dfs -mkdir input
+   hadoop/bin/hdfs dfs -copyFromLocal $INPUT  input
+fi
+
+# Remove output from previous executions
+rm -rf output
+# Execute the script on 
+hadoop/bin/hadoop jar sn.jar SocialNetwork input output
